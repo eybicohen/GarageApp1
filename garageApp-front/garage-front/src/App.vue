8a828d8e12fa68e59331469f6838d25f38766b86
י"ג 4 א' - תוכנה <soft-1@ORTSNG.ORG.IL>
2022-11-23 16:35:29 +0200

    feat(project)

---
+++ garageApp-front/garage-front/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-app-bar
+      absolute
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      prominent
+      src="./assets/pexels-cottonbro-studio-4488637.jpg"
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques-3"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-col class="offset-4">
+        <v-app-bar-title
+          style="font-size: 63px; font-family: Cursive"
+          class="ml-16"
+          >GarageApp</v-app-bar-title
+        >
+      </v-col>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-logout-variant </v-icon>
+      </v-btn>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+import MainPage from "./components/MainPage.vue";
+
+export default {
+  name: "App",
+  data: () => ({
+    //
+  }),
+  components: { MainPage },
+};
+</script>
---
+++ garageApp-front/garage-front/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
